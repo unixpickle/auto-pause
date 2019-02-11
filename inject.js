33c39b6f1461b5d15f8e1fa96b876496bf4c6883
Alex Nichol <unixpickle@gmail.com>
2019-02-11 13:58:27 -0500

    popup for cancelling

--- inject.js
+++ inject.js
@@ -7,15 +7,24 @@
             return;
         }
         window.AUTO_PAUSING = true;
+        const popup = new Popup();
         waitSongEnd().then(() => {
-            window.AUTO_PAUSING = false;
+            popup.cancel();
             pauseSong();
-        });
+        }).catch((e) => {
+            popup.cancel();
+        })
     }
 
     function waitSongEnd() {
         return new Promise((resolve, reject) => {
-            const initMeta = songMetadata();
+            let initMeta;
+            try {
+                initMeta = songMetadata();
+            } catch (e) {
+                reject(e);
+                return;
+            }
             let interval;
             interval = setInterval(() => {
                 if (!window.AUTO_PAUSING) {
@@ -23,7 +32,14 @@
                     reject('cancelled');
                     return;
                 }
-                if (songMetadata() != initMeta) {
+                let newMeta = null;
+                try {
+                    newMeta = songMetadata();
+                } catch (e) {
+                    reject(e);
+                    return;
+                }
+                if (newMeta !== initMeta) {
                     clearInterval(interval);
                     resolve(null);
                 }
@@ -40,6 +56,45 @@
 
     function pauseSong() {
         document.getElementById('player-bar-play-pause').click();
+    }
+
+    class Popup {
+        constructor() {
+            this.element = document.createElement('div');
+            const attrs = {
+                position: 'fixed',
+                right: '30px',
+                top: '30px',
+                width: '200px',
+                textAlign: 'center',
+                backgroundColor: 'white',
+                fontSize: '18px',
+                padding: '10px',
+                boxShadow: '0 0 10px 0 rgba(0, 0, 0, 0.5)',
+            };
+            Object.keys(attrs).forEach((k) => this.element.style[k] = attrs[k]);
+
+            const label = document.createElement('label');
+            label.textContent = 'Pausing after song...';
+            this.element.appendChild(label);
+
+            this.element.appendChild(document.createElement('br'));
+
+            const cancelButton = document.createElement('button');
+            cancelButton.textContent = 'Cancel';
+            cancelButton.style.marginTop = '10px';
+            cancelButton.addEventListener('click', () => this.cancel());
+            this.element.appendChild(cancelButton);
+
+            document.body.appendChild(this.element);
+        }
+
+        cancel() {
+            if (this.element.parentNode) {
+                window.AUTO_PAUSING = false;
+                document.body.removeChild(this.element);
+            }
+        }
     }
 
     runAutopause();
